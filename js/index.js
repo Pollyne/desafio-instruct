40ca0d8846501bac9ee2d9e31c80d4e2ae5b2a49
Pollyne Zunino <pollynezunino@live.com>
2018-07-03 12:12:45 -0300

    Inclusão Filtro Email

--- js/index.js
+++ js/index.js
@@ -1,39 +1,60 @@
+var users = []; // variavel global
+
+//função fetch
 fetch('https://jsonplaceholder.typicode.com/users/')
-  .then(function(response){
-    response.json().then(function(data){
-      
-      for(var key in data) {
-         console.log(data[key].name);
-
-	 var data_inner = data[key].name + 
-		 '<br><ul class=b>' + 
-		 '<li><b>Email:</b> ' + data[key].email + 
-		 '<li><b>Phone:</b> ' + data[key].phone +
-	         '<li><b>Website:</b> ' + data[key].website +
-		 '<li><b>Street:</b> ' + data[key].address.street + 
-		 '<li><b>Suite:</b> ' + data[key].address.suite + 
-		 '<li><b>City:</b> ' + data[key].address.city +
-		 '<li><b>ZipCode:</b> ' + data[key].address.zipcode +
-		 '</ul>'; 
-	 
-	 var addCard = document.createDocumentFragment(),
-		 newDivCard = document.createElement('div'),
-		 newDivInner = document.createElement('div');
-
-   	 newDivCard.id = 'c'+key;
-   	 newDivCard.className = 'card';
-
-   	 newDivInner.id = 'ci'+key;
-   	 newDivInner.className = 'card__inner';
-	 newDivInner.innerHTML = data_inner;
-
-   	 newDivCard.appendChild(newDivInner);
-   	 addCard.appendChild(newDivCard);
-
-	 document.getElementById("myCards").appendChild(addCard);
-      }     
-    });
-  })
-  .catch(function(err){
-    console.error('Failed retrieving information', err);
+  .then(function(response) {
+    response.json().then(function(data) {
+      users = data;
+      renderData(data);
+    })
+      .catch(function(err) {
+        console.error('Failed retrieving information', err);
+      });
   });
+
+function renderData(data) {
+  var el = document.getElementById("myCards");
+  while (el.firstChild) el.removeChild(el.firstChild);
+
+  for (var key in data) {
+    var data_inner = data[key].name +
+      '<br><ul class=b>' +
+      '<li><b>Email:</b> ' + data[key].email +
+      '<li><b>Phone:</b> ' + data[key].phone +
+      '<li><b>Website:</b> ' +data[key].website +
+      '<li><b>Street:</b> ' + data[key].address.street +
+      '<li><b>Suite:</b> ' + data[key].address.suite +
+      '<li><b>City:</b> ' + data[key].address.city +
+      '<li><b>ZipCode:</b> ' + data[key].address.zipcode +
+      '</ul>';
+
+    var addCard = document.createDocumentFragment(),
+      newDivCard = document.createElement('div'),
+      newDivInner = document.createElement('div');
+
+    newDivCard.id = 'c' + key;
+    newDivCard.className = 'card';
+
+    newDivInner.id = 'ci' + key;
+    newDivInner.className = 'card__inner';
+    newDivInner.innerHTML = data_inner;
+
+    newDivCard.appendChild(newDivInner);
+    addCard.appendChild(newDivCard);
+
+    document.getElementById("myCards").appendChild(addCard);
+  }
+}
+
+function filterOnChange() {
+console.log(users);
+  var filter = document.getElementById('myfilter').value;
+	console.log(filter);
+  var filteredUsers = users.filter(function(user) {
+		return user.email.toUpperCase().includes(filter.toUpperCase());
+  });
+
+console.log(filteredUsers);
+  
+	renderData(filteredUsers);
+}
